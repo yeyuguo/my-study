9f256cb03a68c9f8bc6f40827708aae72d5eae7f
yeyuguo <346243440@qq.com>
2020-06-15 17:17:26 +0800

    feat(添加 vue 的响应式实现原理):

--- Frame/Vue/vue-source-mock/index.js
+++ Frame/Vue/vue-source-mock/index.js
@@ -1,16 +1,23 @@
 function defineRective(data, key, val){
-    observe(data)
+    observe(val)
+    var dep = new Dep();
     Object.defineProperty(data, key, {
         get: function(){
             // todo 添加观察者
+            
+            if(Dep.target){
+                dep.add(Dep.target)
+            }
             return val
         },
         set: function(newVal){
             val = newVal
             // todo 触发更新, 性能不好，待优化
+            dep.notify();
         }
     })
 }
+Dep.target = null; // 恢复所有的 target
 
 function observe(data){
     if(!data || typeof data !== 'object') return 
@@ -37,16 +44,16 @@ Dep.prototype.notify = function(){
 
 
 // 实现watcher
-
-function Watcher(vm, exp, cb){
+function Watcher(vm, exp, callback){
     this.vm = vm;
     this.exp = exp;
-    this.cb = cb;
+    this.callback = callback;
     this.value = this.get();
 }
 Watcher.prototype.get = function(){
     Dep.target = this;
     var value = this.vm.data[this.exp] // 该步骤会引起 Object.defineProperty 执行
+    Dep.target = null;
     return value
 }
 
@@ -58,10 +65,47 @@ Watcher.prototype.run = function(){
     var oldVal = this.value
     if(value !== oldVal){
         this.value = value;
-        this.cb(this.vm, value, oldVal)
+        this.callback.call(this.vm, value, oldVal)
     }
+}
+
+
+// 合并所有
+function SelfVue(data, el, exp){
+    this.data = data;
+    // 代理 data 属性
+    // todo 不能理解
+    Object.keys(data).forEach(key=>{
+        this.proxyKey(key)
+    })
+    observe(data)
+    el.innerHTML = this.data[exp];
+    new Watcher(this, exp, function(val, oldVal){
+        el.innerHTML = val
+    })
+    return this;
+}
+
+
+SelfVue.prototype.proxyKey = function(key){
+    var that = this
+    Object.defineProperty(this, key, {
+        enumerable: false,
+        configurable: true,
+        get: function(){
+            return that.data[key]
+        },
+        set: function(val){
+            console.log('val: ', val);
+            that.data[key] = val
+        }
+    })
 }
 
 
 
 
+function CompileElement(el){
+    
+
+}
